ad5e2f68b43ad39d3cc7bbcda89c88decf8e9041
fengzaiyao <2677176483@qq.com>
2022-07-15 21:18:45 +0800

    修改测试用例

--- plugin-mongo-test/src/main/java/io/github/fengzaiyao/plugin/mongo/test/creator/MyDSCreator.java
+++ plugin-mongo-test/src/main/java/io/github/fengzaiyao/plugin/mongo/test/creator/MyDSCreator.java
@@ -1,16 +1,33 @@
 package io.github.fengzaiyao.plugin.mongo.test.creator;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import io.github.fengzaiyao.plugin.mongo.dynamic.creator.DataSourceCreator;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.mongo.MongoClientFactory;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyDSCreator implements DataSourceCreator {
+public class MyDSCreator implements DataSourceCreator, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
     @Override
     public MongoTemplate createDataSource(MongoProperties properties) {
-        // 传递给对应数据源的 MongoProperties 文件了,可根据参数自定义创建过程
-        // 返回 null 的会抛出异常, spring 会检查的
-        return null;
+        ObjectProvider<MongoClientOptions> options = applicationContext.getBeanProvider(MongoClientOptions.class);
+        MongoClient client = new MongoClientFactory(properties, applicationContext.getEnvironment()).createMongoClient(options.getIfAvailable());
+        SimpleMongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(client, properties.getMongoClientDatabase());
+        return new MongoTemplate(mongoDbFactory);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
